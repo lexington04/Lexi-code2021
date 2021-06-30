873aacbd9aabcd1023534e9d662456585ebec543
lexington04 <86678723+lexington04@users.noreply.github.com>
2021-06-30 10:24:45 -0600

    Add files via upload

---
+++ signwave.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plot 
+time = np.arange(0, 10, 0.1);
+
+amplitude =np.sin(time)
+
+plot.plot(time, amplitude)
+
+plot.title('Sign Wave 1')
+
+plot.xlabel('Time')
+
+plot.ylabel('Amplitude = sin(time)')
+
+plot.grid(True, which='both')
+
+plot.axhline(y=0, color='k')
+
+plot.show()
+
